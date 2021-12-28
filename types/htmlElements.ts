864bf020d7d101557ba5361944e4c713cc3c7ad3
Nick <chidojiro@gmail.com>
2021-12-28 20:40:16 +0700

    feat: common types

---
+++ types/htmlElements.ts
@@ -0,0 +1,3 @@
+export type HTMLDivProps = React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement>
+
+export type HTMLInputProps = React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
