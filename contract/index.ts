02f941655e13da4885669e85c91ad09f1b5cc077
Minh Phan <minhphan2603@gmail.com>
2022-02-19 08:59:14 +0700

    fix: typo

--- contract/index.ts
+++ contract/index.ts
@@ -3,6 +3,6 @@ import ABI from './Y2123.json';
 
 export const createContract = (): ethers.Contract => {
   const signer = new ethers.providers.Web3Provider(window.ethereum).getSigner();
-  const contract = new ethers.Contract(process.env.CLAN_CONTRACT!, ABI.abi, signer);
+  const contract = new ethers.Contract(process.env.CLANS_CONTRACT!, ABI.abi, signer);
   return contract;
 };
