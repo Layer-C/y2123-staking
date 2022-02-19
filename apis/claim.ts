a4724ac625fa1a3a352800851b2d15276950a47c
Minh Phan <minhphan2603@gmail.com>
2022-02-19 08:28:00 +0700

    feat: integrate api stake/unstake

--- apis/claim.ts
+++ apis/claim.ts
@@ -5,7 +5,7 @@ const CLAIM_URL = config.BASE_URL + '/claim';
 
 const claim = async (address: string, donate: string) => {
   try {
-    const res = await fetch(CLAIM_URL + `?addr=${address}&donate${donate}`);
+    const res = await fetch(CLAIM_URL + `?addr=${address}&donate=${donate}`);
     return resolveResponse(res);
   } catch (error: any) {
     throw new Error(error);
