59b3456b66803c7f282fb8b80a3cf2878f65588a
Minh Phan <48484833+minhphan2603@users.noreply.github.com>
2022-03-07 13:38:01 +0700

    feat: redirect to dashboard (#15)

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,9 +3,23 @@ import Head from 'next/head';
 import Layout from 'components/Layout';
 import Prose from 'components/Prose';
 import Minting from 'components/Minting';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import { AppLayout } from 'components/AppLayout';
+
+const isRedirectToDashboard = true;
 
 const Home: NextPage = () => {
-  return (
+  const router = useRouter();
+  useEffect(() => {
+    if (isRedirectToDashboard) {
+      router.replace('/dashboard');
+    }
+  }, []);
+
+  return isRedirectToDashboard ? (
+    <AppLayout background='/dashboard-background.png'></AppLayout>
+  ) : (
     <Layout>
       <Prose>
         <Minting />
